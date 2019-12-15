56a15fce24f6e847119e56a328214d9e0998b22e
ptahmose <jbohl@h-quer.de>
2019-12-16 00:50:40 +0100

    update

--- SqliteImgDoc/external/utilities.h
+++ SqliteImgDoc/external/utilities.h
@@ -1,29 +1,31 @@
 #pragma once
 
+#include <cmath>
+
 namespace SlImgDoc
 {
     template <typename t>
     inline bool approximatelyEqual(t a, t b, t epsilon)
     {
-        return fabs(a - b) <= ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+        return std::fabs(a - b) <= ((std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
     }
 
     template <typename t>
     inline bool essentiallyEqual(t a, t b, t epsilon)
     {
-        return fabs(a - b) <= ((fabs(a) > fabs(b) ? fabs(b) : fabs(a))* epsilon);
+        return std::fabs(a - b) <= ((std::fabs(a) > std::fabs(b) ? std::fabs(b) : std::fabs(a))* epsilon);
     }
 
     template <typename t>
     inline bool definitelyGreaterThan(t a, t b, t epsilon)
     {
-        return (a - b) > ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+        return (a - b) > ((std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
     }
 
     template <typename t>
     inline bool definitelyLessThan(t a, t b, t epsilon)
     {
-        return (b - a) > ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+        return (b - a) > ((std::fabs(a) < std::fabs(b) ? std::fabs(b) : std::fabs(a)) * epsilon);
     }
 
 }
